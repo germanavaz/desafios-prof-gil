43a14e55a141862904e57bc2d7205c0e586d730b
germanavaz <gmsvaz@gmail.com>
2023-11-14 22:18:02 -0300

    atualização desafio 5

--- desafio-5/script.js
+++ desafio-5/script.js
@@ -1,11 +1,3 @@
-// Escreva um programa que solicita ao usuário um número inteiro e verifica se esse número atende aos seguintes critérios, sem nenhuma exceção:
-
-// É um número par.
-// É um número positivo (maior que zero).
-// É um número menor que 100.
-
-// O programa deve imprimir a mensagem "O número atende aos critérios.", se o número atender a todos os critérios acima, caso contrário, deve imprimir a mensagem "O número não atende aos critérios.".
-
 
 var numero = prompt('Escreva seu número:');
 var modulo = numero % 2;
