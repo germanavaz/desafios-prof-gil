32e5076ae0f3a833c9ac39a4282d65a0eada7a73
germanavaz <gmsvaz@gmail.com>
2023-11-16 23:04:59 -0300

    atualização script

--- desafio-5/script.js
+++ desafio-5/script.js
@@ -2,7 +2,7 @@
 var numero = prompt('Escreva seu número:');
 var modulo = numero % 2;
 
-if (modulo === 0 && numero >= 0 && numero < 100) {
+if (modulo == 0 & numero >= 0 & numero < 100) {
     alert('O número atende aos critérios.')
 } else {
     alert('O número não atende aos critérios.');
